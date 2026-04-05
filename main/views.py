fb951b8b93f1e6ce28c0c350645cb0ebd68a0618
babafemi <femi-ogundipe@outlook.com>
2026-04-05 16:16:31 +0100

    fix templates issues

--- main/views.py
+++ main/views.py
@@ -30,5 +30,5 @@ def team(request):
 def testimonial(request):
     return render(request, 'testimonial.html')
 
-def error_404(request):
-    return render(request, '404.html')
+def error_404(request, exception=None):
+    return render(request, '404.html', status=404)
